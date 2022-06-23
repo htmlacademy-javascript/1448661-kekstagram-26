b5a7ebf515ba098fe5c3d06e58a4803a9355b93c
Mikhail Samorodov <masamorodov@gmail.com>
2022-06-23 11:48:11 +0300

    Добавляет правки по комментариям к коду

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-import './data.js';
-import './thumbnails-rendering.js';
+import {createPhotosDescriptions} from './data.js';
+import {buildThumbnails} from './thumbnails-rendering.js';
 
+const photosDescriptions = createPhotosDescriptions(25);
+const photosContainer = document.querySelector('.pictures');
+const photosFragment = buildThumbnails(photosDescriptions);
+photosContainer.appendChild(photosFragment);
--- js/thumbnails-rendering.js
+++ js/thumbnails-rendering.js
@@ -1,21 +1,23 @@
-import {createPhotosDescriptions} from './data.js';
-
-const photosDescriptions = createPhotosDescriptions(25);
-
-const photosContainer = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content;
-
-const photosFragment = document.createDocumentFragment();
-
-photosDescriptions.forEach(({url, likes, comments}) => {
+const buildThumbnail = function ({url, likes, comments}) {
+  const pictureTemplate = document.querySelector('#picture').content;
   const photo = pictureTemplate.cloneNode(true);
 
   photo.querySelector('.picture__img').src = url;
   photo.querySelector('.picture__likes').textContent = likes;
   photo.querySelector('.picture__comments').textContent = comments.length;
-  photosFragment.appendChild(photo);
-});
+  return photo;
+};
 
-photosContainer.appendChild(photosFragment);
+const buildThumbnails = function (photosDescriptions) {
+  const photosFragment = document.createDocumentFragment();
+
+  photosDescriptions.forEach(({url, likes, comments}) => {
+    const photo = buildThumbnail({url, likes, comments})
+    photosFragment.appendChild(photo);
+  });
+  return photosFragment;
+};
+
+export {buildThumbnails};
 
 
